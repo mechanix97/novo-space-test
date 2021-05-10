7ee02ac612cc425ac1be996807f2c13377e90c63
Lucas Alexis Rack <lucas_rack@live.com.ar>
2021-05-10 20:24:21 -0300

    upgraded 2'c

--- ej1/sumador.py
+++ ej1/sumador.py
@@ -8,7 +8,7 @@ from random import getrandbits
 
 class Stream(Record):
     def __init__(self, width, **kwargs):
-        Record.__init__(self, [('data', width), ('valid', 1), ('ready', 1)], **kwargs)
+        Record.__init__(self, [('data', signed(width)), ('valid', 1), ('ready', 1)], **kwargs)
 
     def accepted(self):
         return self.valid & self.ready
@@ -45,20 +45,27 @@ class Sumador(Elaboratable):
     def __init__(self, width):
         self.a = Stream(width, name='a')
         self.b = Stream(width, name='b')
-        self.r = Stream(width, name='r')
+        self.r = Stream(width + 1, name='r')
+        self.width = width
 
     def elaborate(self, platform):
         m = Module()
         sync = m.d.sync
         comb = m.d.comb
 
+        print(self.a.data.shape())
+        print(self.b.data.shape())
+        print(self.r.data.shape())
+
         with m.If(self.r.accepted()):
             sync += self.r.valid.eq(0)
 
         with m.If(self.a.accepted() & self.b.accepted()):
+            
             sync += [
                 self.r.valid.eq(1),
-                self.r.data.eq(self.a.data + self.b.data)
+                self.r.data.eq((self.a.data + self.b.data)) # Ya genera un numero 2'c de 9 bits 
+                                                            # Suma 2 2'c de 8 bits
             ]
         comb += self.a.ready.eq((~self.r.valid) | (self.r.accepted()))
         comb += self.b.ready.eq((~self.r.valid) | (self.r.accepted()))
@@ -83,17 +90,47 @@ async def burst(dut):
 
     N = 100
     width = len(dut.a__data)
-    mask = int('1' * width, 2)
+    mask = int('1' * (width+1), 2)
 
     data1 = [getrandbits(width) for _ in range(N)]
     data2 = [getrandbits(width) for _ in range(N)]
-    expected = [(d1 + d2) & mask for (d1,d2) in zip(data1,data2)]
     
+    ### Sumo en 2'c de 9 bits
+    expected = []
+    for (d1,d2) in zip(data1,data2):
+        expected.append(
+            d1 + ((d1 >> width-1)<<width) +
+            d2 + ((d2 >> width-1)<<width) & mask)
+    #convierto d1 y d2 a 2'c de 9 bits
+
     cocotb.fork(stream_input_a.send(data1))
     cocotb.fork(stream_input_b.send(data2))
     
     recved = await stream_output.recv(N)
+
     assert recved == expected
+
+@cocotb.test()
+async def limit_cases(dut):
+    await init_test(dut)
+
+    stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
+    stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
+    stream_output = Stream.Driver(dut.clk, dut, 'r__')
+
+    cocotb.fork(stream_input_a.send([128]))
+    cocotb.fork(stream_input_b.send([128]))
+    
+    recved = await stream_output.recv(1)
+
+    assert recved == [256] 
+
+    cocotb.fork(stream_input_a.send([0]))
+    cocotb.fork(stream_input_b.send([0]))
+    
+    recved = await stream_output.recv(1)
+
+    assert recved == [0]
 
 
 if __name__ == '__main__':
